481ba77d1e587e43bee5f5759ec304c0cfb37a58
David <dluna777@outlook.com>
2025-04-15 19:02:25 -0700

    home buttons redirects to youtube ::temp

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -9,9 +9,9 @@ function HeroSection() {
                 <source src={video} type="video/mp4" />
             </video>
             <div className={styles.content}>
-                <HeroSectionTitle title='The Tunnel' year='2024' />
-                <HeroSectionTitle title='Clockwise' year='2024' />
-                <HeroSectionTitle />
+                <HeroSectionTitle title='The Tunnel' year='2024' link="https://youtu.be/p2JNJF3TxZE?si=BNeYIfdFqWHzDF3A" />
+                <HeroSectionTitle title='Clockwise' year='2024' link="https://youtu.be/oyr1xsTfuTU?si=2jDCGg83oIPNbodZ" />
+                <HeroSectionTitle title='Premire' year='2025' link="#" />
             </div>
         </div>
     );
--- src/components/HeroSection/HeroSectionTitle/HeroSectionTitle.tsx
+++ src/components/HeroSection/HeroSectionTitle/HeroSectionTitle.tsx
@@ -1,8 +1,20 @@
 import style from './HeroSectionTitle.module.css';
 
-function HeroSectionTitle({ title = "Placeholder", year = "Soon" }: { title?: string, year?: string }) {
+function HeroSectionTitle(
+    {
+        title = "Placeholder",
+        year = "Soon",
+        link = "#"
+    }: {
+        title?: string,
+        year?: string,
+        link?: string
+    }) {
+
     return (
-        <h1 className={style.title}> {title} <span className={style.year}>{year}</span></h1>
+        <a href={link}>
+            <h1 className={style.title} > {title} <span className={style.year}>{year}</span></h1>
+        </a>
     );
 }
 
