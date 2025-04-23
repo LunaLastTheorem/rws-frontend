c5fbadabe7c1960a8af987d6330acb0db046819a
David <dluna777@outlook.com>
2025-04-22 18:42:00 -0700

    made herosection dynamic

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -4,21 +4,40 @@ import HeroSectionTitle from './HeroSectionTitle/HeroSectionTitle';
 import clockwiseVid from '/videos/clockwise-preview.mp4';
 import tunnelVid from '/videos/tunnel-preview.mp4';
 import defaultVid from '/videos/default.mp4'
+import Video from './Video'
 
+const videoList: Video[] = [
+    {
+        video: undefined,
+        title: "Premiere",
+        releaseDate: "2025",
+        link: "#"
+    },
+    {
+        video: clockwiseVid,
+        title: "clockwise",
+        releaseDate: "2024",
+        link: "https://youtu.be/oyr1xsTfuTU?si=2jDCGg83oIPNbodZ"
+    },
+    {
+        video: tunnelVid,
+        title: "The Tunnel",
+        releaseDate: "2024",
+        link: "https://youtu.be/p2JNJF3TxZE?si=BNeYIfdFqWHzDF3A"
+    }
+]
 
-const videoMap: Record<string, string> = {
-    'Clockwise': clockwiseVid,
-    'The Tunnel': tunnelVid
-}
 
 function HeroSection() {
     const [currentVideo, setCurrentVideo] = useState(clockwiseVid);
     const videoRef = useRef<HTMLVideoElement>(null);
 
+
     const handleHover = (title: string) => {
-        const video = videoMap[title];
-        if (video) setCurrentVideo(video)
-        else setCurrentVideo(defaultVid)
+        const videoItem = videoList.find(v => v.title === title);
+        const video = videoItem?.video;
+        if (video && window.scrollY === 0) setCurrentVideo(video);
+        else setCurrentVideo(defaultVid);
     }
 
     useEffect(() => {
@@ -34,9 +53,15 @@ function HeroSection() {
                 <source src={currentVideo} type="video/mp4" />
             </video>
             <div className={styles.content}>
-                <HeroSectionTitle onHover={handleHover} title='Premiere' year='2025' link="#" />
-                <HeroSectionTitle onHover={handleHover} title='Clockwise' year='2024' link="https://youtu.be/oyr1xsTfuTU?si=2jDCGg83oIPNbodZ" />
-                <HeroSectionTitle onHover={handleHover} title='The Tunnel' year='2024' link="https://youtu.be/p2JNJF3TxZE?si=BNeYIfdFqWHzDF3A" />
+                {videoList.map((item) => (
+                    <HeroSectionTitle
+                        key={item.title}
+                        onHover={handleHover}
+                        title={item.title}
+                        year={item.releaseDate}
+                        link={item.link}
+                    />
+                ))}
             </div>
         </div>
     );
---
+++ src/components/HeroSection/Video.ts
@@ -0,0 +1,8 @@
+class Video {
+    video: string | undefined
+    title: string | undefined
+    releaseDate: string | undefined
+    link: string | undefined;
+}
+
+export default Video;
